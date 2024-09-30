af19e3d1eea1b0b610817f77a0cea2f62ae709ba
Jürgen Kudla <kudlaj@gmail.com>
2024-09-30 09:11:33 +0200

    mappings

---
+++ goalrev-processor/src/entity/Player.ts
@@ -0,0 +1,66 @@
+import { Entity, Column, PrimaryColumn, ManyToOne, JoinColumn } from "typeorm";
+import { Team } from "./Team"; // Assuming you have a Team entity
+
+@Entity('players')
+export class Player {
+  @PrimaryColumn({ type: 'text' })
+  player_id!: string;
+
+  @ManyToOne(() => Team)
+  @JoinColumn({ name: "team_id" })
+  team!: Team;
+
+  @Column({ type: 'int' })
+  defence!: number;
+
+  @Column({ type: 'int' })
+  speed!: number;
+
+  @Column({ type: 'int' })
+  pass!: number;
+
+  @Column({ type: 'int' })
+  shoot!: number;
+
+  @Column({ type: 'int' })
+  endurance!: number;
+
+  @Column({ type: 'int' })
+  shirt_number!: number;
+
+  @Column({ type: 'text' })
+  preferred_position!: string;
+
+  @Column({ type: 'int' })
+  potential!: number;
+
+  @Column({ type: 'int' })
+  day_of_birth!: number;
+
+  @Column({ type: 'text' })
+  encoded_skills!: string;
+
+  @Column({ type: 'text' })
+  encoded_state!: string;
+
+  @Column({ type: 'boolean', default: false })
+  red_card!: boolean;
+
+  @Column({ type: 'int', default: 0 })
+  injury_matches_left!: number;
+
+  @Column({ type: 'int' })
+  tiredness!: number;
+
+  @Column({ type: 'text' })
+  country_of_birth!: string;
+
+  @Column({ type: 'text' })
+  race!: string;
+
+  @Column({ type: 'boolean', default: false })
+  yellow_card_1st_half!: boolean;
+
+  @Column({ type: 'boolean', default: false })
+  voided!: boolean;
+}
