88ec1dcb2190c314daaa90cfb66d56a7bab5427a
Jürgen Kudla <kudlaj@gmail.com>
2024-10-03 13:43:51 +0200

    refactor mapper

--- goalrev-processor/src/services/MatchService.ts
+++ goalrev-processor/src/services/MatchService.ts
@@ -1,9 +1,8 @@
 import { AppDataSource } from "../db/AppDataSource";
 import { Match, MatchState } from "../db/entity/Match";
 import axios from "axios";
-import { Player } from "../db/entity/Player";
-import { MatchEventRequest, PlayMatchRequest, TacticRequest, TrainingRequest } from "../types";
-import { MatchEvent, Tactics, Training } from "../db/entity";
+import { MatchMapper } from "./mapper/MatchMapper";
+import { PlayMatchRequest } from "../types";
 
 export class MatchService {
 
@@ -59,8 +58,8 @@ export class MatchService {
       verseSeed: match.seed,
       matchStartTime: Number(match.start_epoch),
       skills: [
-        this.calculateTeamSkills(match.homeTeam!.players), // Team 1 skills
-        this.calculateTeamSkills(match.visitorTeam!.players) // Team 2 skills
+        MatchMapper.calculateTeamSkills(match.homeTeam!.players), // Team 1 skills
+        MatchMapper.calculateTeamSkills(match.visitorTeam!.players) // Team 2 skills
       ],
       teamIds: [
         Number(match.homeTeam!.team_id),
@@ -68,106 +67,17 @@ export class MatchService {
       ],
       // Use tactics for both teams
       tactics: [
-        this.mapTacticToRequest(match.homeTeam!.tactics),  // Home team tactics
-        this.mapTacticToRequest(match.visitorTeam!.tactics) // Visitor team tactics
+        MatchMapper.mapTacticToRequest(match.homeTeam!.tactics),  // Home team tactics
+        MatchMapper.mapTacticToRequest(match.visitorTeam!.tactics) // Visitor team tactics
       ],
-      matchEvents: match.matchEvents.map(this.mapMatchEventToRequest),
+      matchEvents: match.matchEvents.map(MatchMapper.mapMatchEventToRequest),
       matchBools: [match.state === MatchState.HALF, true, false, false, false], 
       trainings: [
-        this.mapTrainingToRequest(match.homeTeam!.trainings),  // Home team training
-        this.mapTrainingToRequest(match.visitorTeam!.trainings) // Visitor team training
+        MatchMapper.mapTrainingToRequest(match.homeTeam!.trainings),  // Home team training
+        MatchMapper.mapTrainingToRequest(match.visitorTeam!.trainings) // Visitor team training
       ]
     };
   }
 
-  private mapMatchEventToRequest(matchEvent: MatchEvent): MatchEventRequest {
-    return {
-      minute: matchEvent.minute,
-      type: matchEvent.type,
-      team_id: matchEvent.team_id,
-      primary_player_id: matchEvent.primary_player_id,
-      secondary_player_id: matchEvent.secondary_player_id,
-      manage_to_shoot: matchEvent.manage_to_shoot,
-      is_goal: matchEvent.is_goal
-    };
-  }
   
-
-  private mapTacticToRequest(tactic: Tactics): TacticRequest {
-    return {
-      lineup: [
-        tactic.shirt_0, tactic.shirt_1, tactic.shirt_2, tactic.shirt_3, tactic.shirt_4,
-        tactic.shirt_5, tactic.shirt_6, tactic.shirt_7, tactic.shirt_8, tactic.shirt_9, 
-        tactic.shirt_10
-      ],
-      substitutions: [
-        {
-          shirt: tactic.substitution_0_shirt,
-          target: tactic.substitution_0_target,
-          minute: tactic.substitution_0_minute
-        },
-        {
-          shirt: tactic.substitution_1_shirt,
-          target: tactic.substitution_1_target,
-          minute: tactic.substitution_1_minute
-        },
-        {
-          shirt: tactic.substitution_2_shirt,
-          target: tactic.substitution_2_target,
-          minute: tactic.substitution_2_minute
-        }
-      ],
-      extraAttack: [
-        tactic.extra_attack_1, tactic.extra_attack_2, tactic.extra_attack_3, tactic.extra_attack_4,
-        tactic.extra_attack_5, tactic.extra_attack_6, tactic.extra_attack_7, tactic.extra_attack_8,
-        tactic.extra_attack_9, tactic.extra_attack_10
-      ]
-    };
-
-  }
-
-  private mapTrainingToRequest(training: Training): TrainingRequest {
-    return {
-      specialPlayerShirt: training.special_player_shirt,
-      goalkeepers: {
-        defence: training.goalkeepers_defence,
-        speed: training.goalkeepers_speed,
-        pass: training.goalkeepers_pass,
-        shoot: training.goalkeepers_shoot,
-        endurance: training.goalkeepers_endurance
-      },
-      defenders: {
-        defence: training.defenders_defence,
-        speed: training.defenders_speed,
-        pass: training.defenders_pass,
-        shoot: training.defenders_shoot,
-        endurance: training.defenders_endurance
-      },
-      midfielders: {
-        defence: training.midfielders_defence,
-        speed: training.midfielders_speed,
-        pass: training.midfielders_pass,
-        shoot: training.midfielders_shoot,
-        endurance: training.midfielders_endurance
-      },
-      attackers: {
-        defence: training.attackers_defence,
-        speed: training.attackers_speed,
-        pass: training.attackers_pass,
-        shoot: training.attackers_shoot,
-        endurance: training.attackers_endurance
-      },
-      specialPlayer: {
-        defence: training.special_player_defence,
-        speed: training.special_player_speed,
-        pass: training.special_player_pass,
-        shoot: training.special_player_shoot,
-        endurance: training.special_player_endurance
-      }
-    };
-  }
-
-  private calculateTeamSkills(players: Player[]): string[] {
-    return players.map(player => player.encoded_skills);
-  }
 }
