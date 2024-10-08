4dc6145a3c541b41be4731ef656d5794cf5f7c02
Jürgen Kudla <kudlaj@gmail.com>
2024-10-08 09:38:15 +0200

    verses table

--- goalrev-processor/src/db/entity/Verse.ts
+++ goalrev-processor/src/db/entity/Verse.ts
@@ -1,12 +1,9 @@
-import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn, PrimaryColumn } from "typeorm";
 import { Timezone } from "./Timezone"; // Assuming you have a Timezone entity already defined
 
 @Entity("verses")
 export class Verse {
-  @PrimaryGeneratedColumn({ name: 'verse_id' })
-  verseId!: number;
-
-  @Column({ name: 'verse_number', type: 'int' })
+  @PrimaryColumn({ name: 'verse_number', type: 'int' })
   verseNumber!: number;
 
   @Column({ name: 'verse_timestamp', type: 'timestamp' })
