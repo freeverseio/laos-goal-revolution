181d66bbf8617f353a52d082f2aa8a4a2d2d9c75
Jürgen Kudla <kudlaj@gmail.com>
2024-10-08 09:37:23 +0200

    verses table

--- goalrev-api/db/02_verses.sql
+++ goalrev-api/db/02_verses.sql
@@ -1,13 +1,11 @@
 
 DROP TABLE IF EXISTS public.verses;
 CREATE TABLE public.verses (
-    verse_id SERIAL PRIMARY KEY,
-    verse_number INT NOT NULL,
+    verse_number INT NOT NULL PRIMARY KEY,
     verse_timestamp TIMESTAMP NOT NULL,
     timezone_idx INT NOT NULL,
     root VARCHAR(255) DEFAULT '0',
-    PRIMARY KEY (verse_id),
     FOREIGN KEY (timezone_idx) REFERENCES public.timezones(timezone_idx)
 );
 
-INSERT INTO public.verses (verse_number, verse_timestamp, timezone_idx) VALUES (0, '2024-05-10 18:00:00', 10);
+INSERT INTO public.verses (verse_number, verse_timestamp, timezone_idx) VALUES (1, '2024-05-10 18:00:00', 23);
