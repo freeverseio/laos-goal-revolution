30e7706e44906f33f77c5491c7435ed3c5fcf83e
Jürgen Kudla <kudlaj@gmail.com>
2024-10-10 09:41:28 +0200

    create tables

--- goalrev-api/db/01_create_tables.sql
+++ goalrev-api/db/01_create_tables.sql
@@ -1,3 +1,143 @@
+
+CREATE TYPE public."auction_state" AS ENUM (
+	'started',
+	'failed',
+	'cancelled',
+	'ended',
+	'asset_frozen',
+	'paying',
+	'withadrable_by_seller',
+	'withadrable_by_buyer',
+	'validation');
+
+-- DROP TYPE public."bid_state";
+
+CREATE TYPE public."bid_state" AS ENUM (
+	'accepted',
+	'paying',
+	'paid',
+	'failed');
+
+-- DROP TYPE public."match_event_type";
+
+CREATE TYPE public."match_event_type" AS ENUM (
+	'attack',
+	'yellow_card',
+	'red_card',
+	'injury_soft',
+	'injury_hard',
+	'substitution');
+
+-- DROP TYPE public."match_state";
+
+CREATE TYPE public."match_state" AS ENUM (
+	'begin',
+	'half',
+	'end',
+	'cancelled');
+
+-- DROP TYPE public."offer_state";
+
+CREATE TYPE public."offer_state" AS ENUM (
+	'started',
+	'failed',
+	'cancelled',
+	'ended',
+	'accepted');
+
+-- DROP TYPE public."playstore_order_state";
+
+CREATE TYPE public."playstore_order_state" AS ENUM (
+	'open',
+	'acknowledged',
+	'complete',
+	'refunding',
+	'refunded',
+	'failed');
+
+-- DROP TYPE public."_auction_state";
+
+CREATE TYPE public."_auction_state" (
+	INPUT = array_in,
+	OUTPUT = array_out,
+	RECEIVE = array_recv,
+	SEND = array_send,
+	ANALYZE = array_typanalyze,
+	ALIGNMENT = 4,
+	STORAGE = any,
+	CATEGORY = A,
+	ELEMENT = public."auction_state",
+	DELIMITER = ',');
+
+-- DROP TYPE public."_bid_state";
+
+CREATE TYPE public."_bid_state" (
+	INPUT = array_in,
+	OUTPUT = array_out,
+	RECEIVE = array_recv,
+	SEND = array_send,
+	ANALYZE = array_typanalyze,
+	ALIGNMENT = 4,
+	STORAGE = any,
+	CATEGORY = A,
+	ELEMENT = public."bid_state",
+	DELIMITER = ',');
+
+-- DROP TYPE public."_match_event_type";
+
+CREATE TYPE public."_match_event_type" (
+	INPUT = array_in,
+	OUTPUT = array_out,
+	RECEIVE = array_recv,
+	SEND = array_send,
+	ANALYZE = array_typanalyze,
+	ALIGNMENT = 4,
+	STORAGE = any,
+	CATEGORY = A,
+	ELEMENT = public."match_event_type",
+	DELIMITER = ',');
+
+-- DROP TYPE public."_match_state";
+
+CREATE TYPE public."_match_state" (
+	INPUT = array_in,
+	OUTPUT = array_out,
+	RECEIVE = array_recv,
+	SEND = array_send,
+	ANALYZE = array_typanalyze,
+	ALIGNMENT = 4,
+	STORAGE = any,
+	CATEGORY = A,
+	ELEMENT = public."match_state",
+	DELIMITER = ',');
+
+-- DROP TYPE public."_offer_state";
+
+CREATE TYPE public."_offer_state" (
+	INPUT = array_in,
+	OUTPUT = array_out,
+	RECEIVE = array_recv,
+	SEND = array_send,
+	ANALYZE = array_typanalyze,
+	ALIGNMENT = 4,
+	STORAGE = any,
+	CATEGORY = A,
+	ELEMENT = public."offer_state",
+	DELIMITER = ',');
+
+-- DROP TYPE public."_playstore_order_state";
+
+CREATE TYPE public."_playstore_order_state" (
+	INPUT = array_in,
+	OUTPUT = array_out,
+	RECEIVE = array_recv,
+	SEND = array_send,
+	ANALYZE = array_typanalyze,
+	ALIGNMENT = 4,
+	STORAGE = any,
+	CATEGORY = A,
+	ELEMENT = public."playstore_order_state",
+	DELIMITER = ',');
 -- public.params definition
 
 -- Drop table
