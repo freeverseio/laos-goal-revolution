03227aa31734d6c34e48ec2513583d8502d88011
dpla <dpla@freeverse.io>
2024-10-10 12:45:38 +0200

    use factory

--- goalrev-processor/src/controller/LeagueController.ts
+++ goalrev-processor/src/controller/LeagueController.ts
@@ -1,28 +1,15 @@
 import { Post, JsonController, Body } from "routing-controllers";
 import { LeagueService } from "../services/LeagueService";
 import { UpdateLeagueLeaderboardInput, UpdateLeagueLeaderboardsInput } from "../types";
-import { VerseService } from "../services/VerseService";
-import { MatchService } from "../services/MatchService";
-import { TeamService } from "../services/TeamService";
-import { PlayerService } from "../services/PlayerService";
-import { MatchEventService } from "../services/MatchEventService";
+import { LeagueFactory } from "../factories/LeagueFactory";
 
 @JsonController("/league")
-export class LeagueController {
+export class LeagueController {    
+
     private leagueService: LeagueService;
-    private matchService: MatchService;
-    private verseService: VerseService;
-    private teamService: TeamService;
-    private playerService: PlayerService;
-    private matchEventService: MatchEventService;
 
     constructor() {
-        this.playerService = new PlayerService();
-        this.teamService = new TeamService();
-        this.matchEventService = new MatchEventService(); 
-        this.verseService = new VerseService(); 
-        this.matchService = new MatchService(this.playerService, this.teamService, this.matchEventService, this.verseService); 
-        this.leagueService = new LeagueService(this.matchService, this.verseService);
+        this.leagueService = LeagueFactory.createLeagueService();
     }
 
     @Post("/updateLeaderboards")
