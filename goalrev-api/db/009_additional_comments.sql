5254e43688c1278e5dbe09928c0566ed1c1f1785
Christian Cuaresma <christian@freeverse.io>
2024-11-08 12:20:45 +0100

    additional smart tags to hide mutations

---
+++ goalrev-api/db/009_additional_comments.sql
@@ -0,0 +1,14 @@
+
+comment on table auctions is E'@omit create,update,delete';
+comment on table auctions_histories is E'@omit create,update,delete';
+comment on table bids is E'@omit create,update,delete';
+comment on table bids_histories is E'@omit create,update,delete';
+comment on table inbox is E'@omit create,update,delete';
+comment on table offers is E'@omit create,update,delete';
+comment on table offers_histories is E'@omit create,update,delete';
+comment on table playstore_orders is E'@omit create,update,delete';
+comment on table playstore_orders_histories is E'@omit create,update,delete';
+comment on table shop_items is E'@omit create,update,delete';
+comment on table tactics_histories is E'@omit create,update,delete';
+comment on table trainings_histories is E'@omit create,update,delete';
+comment on table team_props_histories is E'@omit create,update,delete'
